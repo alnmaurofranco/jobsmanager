95514be82dd936f6e15ea6134036884c9479cc5b
alnmaurofranco <jefeson23@live.com>
2021-07-14 23:58:09 -0300

    :rocket: deploy vercel

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333/api'
+  baseURL: process.env.NODE_ENV === 'production'
+      ? 'https://api-jobsmanager.herokuapp.com/api'
+      : 'http://localhost:3333/api'
 })
 
 export const fetched = axios.create({
